8f2b9f5dc6cbc20615fca2e9a2b9c60d46bc129a
Aravinth SS <aravinthssivakumar@outlook.com>
2024-08-15 18:25:54 +0530

    puzzle implemented but has a ton of bugs

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
+
     public void loadNextLevel(int index)
     {
+        //StartCoroutine(LoadLevel(index));
         SceneManager.LoadScene(index);
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
