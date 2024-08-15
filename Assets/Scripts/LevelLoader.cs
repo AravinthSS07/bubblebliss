984fad11b61d082ffec77e653d839752d61d8d75
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2024-08-15 16:01:08 +0530

    background map game

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void nextScene(int next)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(next);
     }
 }
