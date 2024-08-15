5e2cd7fe6ea09dd0465235099e9ec8e3629d9b85
Aravinth SS <aravinthssivakumar@outlook.com>
2024-08-15 18:31:48 +0530

    Revert "Merge branch 'v2' into gowtham"
    This reverts commit de54ab16199b436af369b82d0ff51df36a60915e, reversing
    changes made to 244810a22bac8046da64fe74d67468591f60877a.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,21 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public Animator transition;
-    public float transitionTime = 1f;
-
-    public void loadNextLevel(int index)
+    public void nextScene(int next)
     {
-        //StartCoroutine(LoadLevel(index));
-        SceneManager.LoadScene(index);
-    }
-
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
-        SceneManager.LoadScene(levelIndex);
+        SceneManager.LoadScene(next);
     }
 }
