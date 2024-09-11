943bc173bede41192881b35b5398095a21aaab1f
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2024-09-11 23:27:26 +0530

    burst game

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject germPrefab;
     public float spawnInterval = 2f;
+    private bool isGameStarted = false; // Variable to track whether the game has started
 
     void Start()
     {
+        // Do not start spawning germs until the game begins
+    }
+
+    // Function to be called when the game starts
+    public void StartSpawning()
+    {
+        isGameStarted = true;
         InvokeRepeating("SpawnGerm", 0f, spawnInterval);
     }
 
     void SpawnGerm()
     {
-        float screenWidth = Camera.main.aspect * Camera.main.orthographicSize;
-        float screenHeight = Camera.main.orthographicSize;
+        if (isGameStarted)
+        {
+            float screenWidth = Camera.main.aspect * Camera.main.orthographicSize;
+            float screenHeight = Camera.main.orthographicSize;
 
-        Vector2 spawnPosition = new Vector2(Random.Range(-screenWidth, screenWidth), Random.Range(-screenHeight, screenHeight));
-        Instantiate(germPrefab, spawnPosition, Quaternion.identity);
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenWidth, screenWidth), Random.Range(-screenHeight, screenHeight));
+            Instantiate(germPrefab, spawnPosition, Quaternion.identity);
+        }
     }
-
 }
