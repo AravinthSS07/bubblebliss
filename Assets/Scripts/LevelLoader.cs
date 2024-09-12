fb30f507340f3f8e89d8f4e80816b69aebd20d6d
Aravinth SS <aravinthssivakumar@outlook.com>
2024-09-12 11:07:36 +0530

    level loader started

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,14 +1,26 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class LevelLoader : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    public void nextScene(int value)
+    public Animator transitionAnimator;
+    public float transitionTime = 1f;
+
+    // Call this method to load a scene by its index
+    public void LoadScene(int sceneIndex)
     {
-        SceneManager.LoadScene(value);
+        StartCoroutine(LoadSceneWithTransition(sceneIndex));
+    }
+
+    IEnumerator LoadSceneWithTransition(int sceneIndex)
+    {
+        // Trigger the transition animation
+        transitionAnimator.SetTrigger("Start");
+
+        // Wait for the animation to finish
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load the scene
+        SceneManager.LoadScene(sceneIndex);
     }
 }
