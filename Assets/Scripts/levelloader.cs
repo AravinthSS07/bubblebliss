e8b5e8d0a1e39ece2151865742e8a122ab3bb82d
Aravinth SS <aravinthssivakumar@outlook.com>
2024-09-13 14:35:06 +0530

    started new version of level loader

--- Assets/Scripts/levelloader.cs
+++ Assets/Scripts/levelloader.cs
@@ -15,7 +15,7 @@ public class levelloader : MonoBehaviour
 
     IEnumerator switchScene(int index)
     {
-
+        animator.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);
         UnityEngine.SceneManagement.SceneManager.LoadScene(index);
     }
